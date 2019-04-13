7375dfdc1c9d234c0fd06b3073035ab5b5e213dc
Abhishek Kumar <akbittu@gmail.com>
2019-04-13 07:43:53 +0530

    Added basic sample codebase

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # kandy-cpaas2-sample-address-book
+
+### Directory and Address Book App
+
+This app is used to add contacts in your address book and then search in your directory if the user name exists or not based on various search parameters (e.g. Name(first or last), First Name, Last Name and Phone Number).
+
+1. Create an account on **AT&T** portal via [Register now for a free account](https://nvs-apimarket.kandy.io/signup).
+2. After signup get the following 
+   - *clientId* 
+   - *emailId* 
+   - *password*
+3. If you are an existing user, please [Log in to AT&T API Marketplace](https://nvs-apimarket.kandy.io/login).
+4. Download *kandy.js* from [Developer documentation - SDKs](https://nvs-apimarket.kandy.io/developer/sdks/javascript)
+5. Login using your credentials (Account ClientID, EmailID and Password).
+6. After successful login, Navigate to AddressBook for adding more contacts in your directory
+7. Enter the following details for required fields given below :
+    - *ID (it should be unique for every contacts added)* 
+    - *Primary Contact* 
+    - *First Name*
+    - *Last Name* 
+    - *Home PhoneNumber* 
+    - *Business PhoneNumber*
+    - *Email ID* 
+    - *Buddy (click on checkbox, it should always br true)*
+8. Click on "Add" button to add the contacts in the list.
+9. You can also update added contacts if any field is missing or needs alteration, by clicking on "update" button.
+10. After adding contacts, you can view the list of existing contacts in your list, by clicking on "Refresh Contacts" button. A list of all recently added and existing users will appear in the dropdown list named as "Address Book Contacts".
+11. You can also remove/delete any existing users, by selecting the unique contact Id from the dropdown list and then click on "Remove" button.
+12. You can also clear all input fields by clicking on "Clear" button.
+13. Navigate again to Directory Part, to check if the user's email id and name exists in the directory.
+14. Enter UserID ex: janedoe@somedomain.com ([userId]@[domain]), to check if any user exists in the directory with same id or not by clicking on "Fetch User" button.
+15. You can also search any contact by filtering the Name(First or Last), First Name, Last Name or Phone Number, by clicking on "Search Directory" button. It will get list of all the contacts based on various search parameters.
+16. You can also search yourself by clicking on "Fetch Self" button which return all details of the present logged in user.
+
+### Development
+
+To setup the project repository, run these commands
+
+```
+git clone https://github.com/hclsampleapps/kandy-cpaas2-sample-address-book.git
+cd kandy-cpaas2-sample-address-book
+```
+
+Then, open ```index.html``` in the browser to view the app.
+
+#### Branching strategy
+
+To learn about the branching strategy, contribution & coding conventions followed in the project, please refer [GitFlow based branching strategy for your project repository](https://gist.github.com/ribbon-abku/10d3fc1cff5c35a2df401196678e258a)
---
+++ src/js/app.js
@@ -0,0 +1,419 @@
+/**
+ * CPaaS Authentication Demo
+ */
+var changeView;
+var showhideView;
+var serverBase;
+var mHostUrl;
+var client;
+const tokenAPI = '/cpaas/auth/v1/token'
+
+whenReady(function() {
+    changeView = new ChangeView();
+    changeView.showDirectoryView();
+    changeView.showPasswordGrant();
+});
+
+class ChangeView {
+    constructor() {
+        this.directoryView = document.getElementById('directoryView');
+        this.addressBookView = document.getElementById('addressBookView');
+
+        this.accountPasswordGrantView = document.getElementById('passwordID');
+        this.accountClientCredentialsView = document.getElementById('clientCredID');
+
+        this.accountPasswordGrantradio = document.getElementById('passwordGrant');
+        this.accountPasswordGrantradio.addEventListener('click', (evt) => this.showPasswordGrant(evt));
+
+        this.accountClientCredentialsradio = document.getElementById('clientCred');
+        this.accountClientCredentialsradio.addEventListener('click', (evt) => this.showClientCredentials(evt));
+
+        this.directoryRadio = document.getElementById('directory');
+        this.directoryRadio.addEventListener('click', (evt) => this.showDirectoryView(evt));
+
+        this.addressBookRadio = document.getElementById('addressBook');
+        this.addressBookRadio.addEventListener('click', (evt) => this.showAddressBookView(evt));
+    }
+
+    showPasswordGrant() {
+        Effect.hide(this.accountClientCredentialsView);
+        Effect.show(this.accountPasswordGrantView);
+    }
+
+    showClientCredentials() {
+        Effect.show(this.accountClientCredentialsView);
+        Effect.hide(this.accountPasswordGrantView);
+    }
+    showDirectoryView() {
+        Effect.hide(this.addressBookView);
+        Effect.show(this.directoryView);
+    }
+
+    showAddressBookView() {
+        Effect.hide(this.directoryView);
+        Effect.show(this.addressBookView);
+    }
+}
+
+function initClient() {
+    let mServerUrl = document.getElementById("serverUrl").value;
+    mHostUrl = new URL(mServerUrl).host;
+    console.log(mHostUrl);
+    client = Kandy.create({
+        subscription: {
+            expires: 3600
+        },
+        // Required: Server connection configs.
+        authentication: {
+            server: {
+                base: mHostUrl
+            },
+            clientCorrelator: 'sampleCorrelator'
+        }
+    })
+
+    // Assign callback functions to address book events
+    client.on('contacts:change', () => {
+        updateContactsList()
+    })
+
+    client.on('contacts:new', contact => {
+        updateContactsList()
+    })
+
+    // Assign functions to receive directory events
+    client.on('directory:change', () => {
+        updateUserList()
+    })
+
+    // Assign functions to receive directory-related error events
+    client.on('directory:error', error => {
+        const logMessage = 'An error occured: ' + error
+        log(logMessage)
+    })
+}
+
+/**
+ * Creates a form body from an dictionary
+ */
+function createFormBody(paramsObject) {
+    const keyValuePairs = Object.entries(paramsObject).map(
+        ([key, value]) => encodeURIComponent(key) + '=' + encodeURIComponent(value)
+    )
+    return keyValuePairs.join('&')
+}
+
+/**
+ * Gets the tokens necessary for authentication to CPaaS
+ */
+async function getTokensByPasswordGrant({
+    clientId,
+    username,
+    password
+}) {
+    const cpaasAuthUrl = constructServerUrl();
+    const formBody = createFormBody({
+        client_id: clientId,
+        username,
+        password,
+        grant_type: 'password',
+        scope: 'openid'
+    })
+    // POST a request to create a new authentication access token.
+    const fetchResult = await fetch(cpaasAuthUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formBody
+    })
+    // Parse the result of the fetch as a JSON format.
+    const data = await fetchResult.json()
+    return {
+        accessToken: data.access_token,
+        idToken: data.id_token
+    }
+}
+async function loginByPasswordGrant() {
+    initClient();
+    const clientId = document.getElementById('clientId').value
+    const userEmail = document.getElementById('userEmail').value
+    const password = document.getElementById('password').value
+    try {
+        const tokens = await getTokensByPasswordGrant({
+            clientId,
+            username: userEmail,
+            password
+        })
+
+        log('Successfully logged in as ' + userEmail)
+
+        client.setTokens(tokens)
+
+    } catch (error) {
+        log('Error: Failed to get authentication tokens. Error: ' + error)
+    }
+}
+
+async function getTokensByClientCredGrant({
+    client_id,
+    client_secret
+}) {
+
+    const cpaasAuthUrl = constructServerUrl();
+    const formBody = createFormBody({
+        client_id,
+        client_secret,
+        grant_type: 'client_credentials',
+        scope: 'openid regular_call'
+    })
+
+    // POST a request to create a new authentication access token.
+    const fetchResult = await fetch(cpaasAuthUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formBody
+    })
+    // Parse the result of the fetch as a JSON format.
+    const data = await fetchResult.json();
+
+    return {
+        accessToken: data.access_token,
+        idToken: data.id_token
+    }
+}
+
+async function loginByClientCred() {
+    initClient();
+    const privateKey = document.getElementById('privateKey').value
+    const privateSecret = document.getElementById('privateSecret').value
+
+    try {
+        const tokens = await getTokensByClientCredGrant({
+            client_id: privateKey,
+            client_secret: privateSecret
+        })
+        client.setTokens(tokens)
+        log('Successfully logged in with project User ' + privateKey)
+    } catch (error) {
+        log('Error: Failed to get authentication tokens. Error: ' + error)
+    }
+}
+
+function constructServerUrl() {
+    let cpaasUrl;
+    let enteredBaseUrl = document.getElementById("serverUrl").value
+    if (enteredBaseUrl.trim() !== "") {
+        serverBase = enteredBaseUrl.trim()
+    }
+    cpaasUrl = serverBase + tokenAPI
+    return cpaasUrl;
+}
+
+/**
+ * Address Book functions
+ **/
+// Add a contact to the Address Book
+function addContact() {
+    let contact = getContactFormData()
+    if (!contact.contactId) {
+        log('Cannot add a contact to the Address Book without a contact ID!')
+    } else {
+        client.contacts.add(getContactFormData())
+    }
+}
+
+// Remove a contact from the Address Book
+function removeContact() {
+    let contactId = getSelectedContactId()
+    client.contacts.remove(contactId)
+}
+
+// Update the details of a contact in the Address Book
+function updateContact() {
+    const contact = getContactFormData()
+    if (!contact.contactId) {
+        log('Cannot update a contact with no contact ID!')
+    } else {
+        client.contacts.update(contact.contactId, contact)
+    }
+}
+
+// Retrieve all contacts from the Address Book
+function refreshContacts() {
+    client.contacts.refresh()
+}
+
+// Clear all input fields from Address Book
+function clearContact() {
+    document.getElementById('id').value = "";
+    document.getElementById('primary-contact').value = "";
+    document.getElementById('firstN').value = "";
+    document.getElementById('lastN').value = "";
+    document.getElementById('emailN').value = "";
+    document.getElementById('home-phone').value = "";
+    document.getElementById('business-phone').value = "";
+}
+
+// Clear all input fields from Directory
+function clearContactForm() {
+    document.getElementById('user-id').value = "";
+    document.getElementById('filter-value').value = "";
+}
+
+// Get data from the contact form
+function getContactFormData() {
+    const contactId = document.getElementById('id').value
+    const primaryContact = document.getElementById('primary-contact').value
+    const firstName = document.getElementById('firstN').value
+    const lastName = document.getElementById('lastN').value
+    const emailAddress = document.getElementById('emailN').value
+    const homePhoneNumber = document.getElementById('home-phone').value
+    const businessPhoneNumber = document.getElementById('business-phone').value
+    const buddy = document.getElementById('buddy').checked
+
+    return { contactId, primaryContact, firstName, lastName, emailAddress, homePhoneNumber, businessPhoneNumber, buddy }
+}
+
+
+
+// Helper function to update the contacts select list
+function updateContactsList() {
+    const select = document.getElementById('contactDropDown')
+    select.innerHTML = ''
+    const contacts = client.contacts.getAll()
+    for (let contactId in contacts) {
+        for (let opt of select.options) {
+            if (opt.value === contactId) {
+                select.removeChild(opt)
+            }
+        }
+        var opt = document.createElement('option')
+        opt.value = opt.text = contactId
+        select.appendChild(opt)
+        if (select.options.length === 1) {
+            renderContact()
+        }
+    }
+}
+
+// Helper function to render a contact and update the contact form
+function renderContact(id) {
+    const contactId = id || getSelectedContactId()
+
+    if (contactId) {
+        const contact = client.contacts.get(contactId)
+        updateContactForm(contact)
+        const contactDisplay = document.getElementById('display')
+        let text = '<h5>Contact Info</h5>'
+        contactDisplay.innerHTML = ''
+        Object.keys(contact).forEach(
+            key => (text += '<li><b>' + key + '</b>: <i>' + contact[key] + '</i></li>')
+        )
+
+        contactDisplay.innerHTML = '<ul>' + text + '</ul>';
+    }
+}
+
+// Get the contact ID from the selected option in the contact select list
+function getSelectedContactId() {
+    return document.getElementById('contactDropDown').value
+}
+
+// Update the contact form with data from a contact
+function updateContactForm(contact) {
+    document.getElementById('primary-contact').value = contact.primaryContact
+    document.getElementById('id').value = contact.contactId
+    document.getElementById('firstN').value = contact.firstName
+    document.getElementById('lastN').value = contact.lastName
+    document.getElementById('emailN').value = contact.emailAddress
+    document.getElementById('home-phone').value = contact.homePhoneNumber
+    document.getElementById('business-phone').value = contact.businessPhoneNumber
+    document.getElementById('buddy').checked = contact.buddy === 'true'
+}
+
+/**
+ * Directory functions
+ **/
+
+// Fetch one user from the directory
+function fetchUser() {
+    const userId = document.getElementById('user-id').value
+    client.user.fetch(userId)
+}
+
+// Search the directory for users
+function searchDirectory() {
+    const filterValue = document.getElementById('filter-value').value
+    const filterType = document.querySelector('input[name=filter-key]:checked').value
+    // intially it was written "document.querySelector('input[name=filter-type]:checked').value"
+
+    client.user.search({
+        [filterType]: filterValue
+    })
+}
+
+// Fetch your own user information
+function fetchSelf() {
+    client.user.fetchSelfInfo()
+    console.log('fetched self -----', document.getElementById('email').value);
+}
+
+
+// Helper function to update the contacts select list
+function updateUserList() {
+    const select = document.getElementById('userDropDown')
+    const contactId = document.getElementById('id').value // created reference for contactId locally
+    select.innerHTML = ''
+    const users = client.user.getAll()
+    for (let userId in users) {
+        for (let opt of select.options) {
+            if (opt.value === userId) {
+                select.removeChild(opt)
+            }
+        }
+        var opt = document.createElement('option')
+        opt.value = opt.text = contactId; // "Error: contactId was not defined"
+        select.appendChild(opt)
+        if (select.options.length === 1) {
+            renderUser(userId)
+        }
+    }
+}
+
+// Helper function to render a contact and update the user form
+function renderUser(id) {
+    const userId = id || getSelectedUser()
+
+    if (userId != undefined && userId != "") {
+        const user = client.user.get(userId)
+        if (user != undefined && user != "") {
+            const userDisplay = document.getElementById('display')
+            let text = '<h5>User Info</h5>'
+            userDisplay.innerHTML = ''
+            Object.keys(user).forEach(
+                key => (text += '<li><b>' + key + '</b>: <i>' + user[key] + '</i></li>')
+            )
+
+            userDisplay.innerHTML = '<ul>' + text + '</ul>';
+        } else {
+            log('User not found');
+        }
+    }
+}
+
+// Get the contact ID from the selected option in the contact select list
+function getSelectedUser() {
+    return document.getElementById('userDropDown').value
+}
+
+// Utility function for appending messages to the message div.
+function log(message) {
+    console.log(message);
+    document.getElementById('terminal').innerHTML += '<p>' + message + '</p>';
+}
+
+const directoryForm = document.getElementById('directory')
+const addressBookForm = document.getElementById('addressBook')
